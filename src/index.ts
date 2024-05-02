1a3ed178dc0495991f12dbc253596ead4b39e175
Mike Street <mikestreety@gmail.com>
2024-05-02 08:49:01 +0100

    refactor(src): Make reporters last

--- src/index.ts
+++ src/index.ts
@@ -44,6 +44,17 @@ module.exports = (
 		retries: process.env.CI ? 2 : 0,
 		/* Opt out of parallel tests on CI. */
 		workers: process.env.CI ? 1 : undefined,
+		
+		/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+		use: {
+			/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+			trace: 'on-first-retry',
+			screenshot: 'only-on-failure'
+		},
+
+		/* Configure projects for major browsers */
+		projects,
+
 		/* Reporter to use. See https://playwright.dev/docs/test-reporters */
 		reporter: (
 			process.env.CI ?
@@ -163,18 +174,6 @@ module.exports = (
 						}
 					]
 				]
-		),
-		/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-		use: {
-			/* Base URL to use in actions like `await page.goto('/')`. */
-			// baseURL: 'http://127.0.0.1:3000',
-
-			/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-			trace: 'on-first-retry',
-			screenshot: 'only-on-failure'
-		},
-
-		/* Configure projects for major browsers */
-		projects
+		)
 	};
 }
