8c96bd32824d835ebd3c6fb5c3d479efd79c389c
Mike Street <mikestreety@gmail.com>
2025-02-13 20:11:20 +0000

    fix(fixture): Resolve function incompatibility

--- src/fixtures.ts
+++ src/fixtures.ts
@@ -1,7 +1,7 @@
 import type { FrameworkTest } from "./types.js";
 
 import { test as playwrightTest } from "@playwright/test";
-import { matchHostToEnv } from "./utils.js";
+import { matchUrlHostToEnv } from "./utils.js";
 
 /**
  * Test
@@ -22,7 +22,7 @@ export const test = playwrightTest.extend< FrameworkTest >({
 			// Get the URL we're going to
 			const url = request.url(),
 				// Run it against the match functions
-				matchedHost = matchHostToEnv(hosts, url);
+				matchedHost = matchUrlHostToEnv(hosts, url);
 
 			// If they are different
 			if (url !== matchedHost) {
@@ -47,7 +47,6 @@ export const test = playwrightTest.extend< FrameworkTest >({
 	},
 
 	// Overwrite the page
-	// Overwrite the page
 	page: async ({ page, hosts }, use) => {
 		// Create a reference of the original goto
 		const goto = page.goto.bind(page);
@@ -55,13 +54,13 @@ export const test = playwrightTest.extend< FrameworkTest >({
 		// Overwrite the goto function
 		page.goto = async (url: string, options?) => {
 			// Define URL with correct host
-			url = (matchHostToEnv(hosts, url) ?? url)
+			url = (matchUrlHostToEnv(hosts, url) ?? url)
 
 			// Carry on with the original page.goto
-			await goto(url, options);
+			return goto(url, options);
 		}
 
 		// Use the page
 		await use(page);
-	},
+	}
 });
