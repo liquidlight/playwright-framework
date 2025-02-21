c8ed88887751ee04667e1a7cb1109169727383d6
Mike Street <mikestreety@gmail.com>
2025-02-21 14:57:22 +0000

    feat!: Remove legacy functions
    Was going to keep this as a non-breaking change but makes sense to do as a major release

--- src/index.ts
+++ src/index.ts
@@ -23,13 +23,3 @@ export { default as typo3site } from './typo3site.js';
 
 // Utils
 export { defaultDevices, getEnv } from './utils.js';
-
-/**
- * Legacy
- */
-export { default as framework } from './_legacy/framework.js';
-
-export { typo3Config } from './_legacy/utils.js';
-
-// TYPO3 configuration
-export { default as testbed } from './_legacy/testbed.js';
