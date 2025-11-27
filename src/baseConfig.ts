467f3b4613f8ecc8223ad73ab0ebf98aa3214828
Mike Street <mike@liquidlight.co.uk>
2025-11-27 15:30:40 +0000

    fix(src): Resolve incorrect dirname variable

--- src/baseConfig.ts
+++ src/baseConfig.ts
@@ -1,6 +1,11 @@
 import type { FrameworkTestConfig } from './types.js';
 
 import { execSync } from 'child_process';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export const baseConfig: FrameworkTestConfig = {
 	// Projects
